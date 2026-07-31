9204505c8073faae23ed63c59338522e75fc5366
Abhishek-10hue <ehtabhishek@eternalhightech.com>
2026-07-31 16:21:52 +0530

    spatie roles and permission code

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $permissions = [
+            'dashboard-view',
+            'patient-create',
+            'patient-edit',
+            'patient-delete',
+            'doctor-create',
+            'doctor-edit',
+            'doctor-delete',
+            'TEST-PERMISSION'
+        ];
+
+        foreach($permissions as $permission){
+            Permission::firstOrCreate([
+                'name'=>$permission,
+                'guard_name' => 'web',
+            ]);
+        }
+    }
+}
