e96aa34b9b7e2345da1b18450b496f3a16971bf0
DevilShekhar <shekhar@eternalhightech.com>
2026-08-01 09:57:46 +0530

    web file changes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DesignerDashboardController;
 use App\Http\Controllers\admin\UserController;
 use App\Http\Controllers\admin\RolesController;
@@ -11,21 +10,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('admin.dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::middleware(['auth'])->group(function () {
-    Route::get('dashboard', [DesignerDashboardController::class, 'index'])->name('dashboard');
-   Route::resource('users', UserController::class);
-   Route::resource('roles', RolesController::class);
+    Route::get('/dashboard', [DesignerDashboardController::class, 'index'])
+        ->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+    Route::resource('users', UserController::class);
+    Route::resource('roles', RolesController::class);
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
-});
